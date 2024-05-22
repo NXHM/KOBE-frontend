4b369ba7980a0cb68a0563b124fc09315637dfd1
nxhm2013 <nxhm2013@gmail.com>
2024-05-22 17:50:03 +0000

    pantalla de inicio

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/constants/colors.dart';
+
+class WelcomeScreen extends StatelessWidget{
+  const WelcomeScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          ClipOval(
+            child: Image.asset('assets/images/Kobey-logo.jpeg',
+            fit: BoxFit.cover,
+            width: 200.0,
+            height: 200.0,
+            ),
+          ),
+          const SafeArea(
+            child: Text("Welcome nigga")
+            )
+
+        ]
+      )
+    );
+  }
+}
