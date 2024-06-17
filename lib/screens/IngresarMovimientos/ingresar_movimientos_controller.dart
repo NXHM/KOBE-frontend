270cb23c63437d1f6659f275567e7cb9738a2187
Flavio Zegarra <flaviozegarra00@gmail.com>
2024-06-17 18:20:01 -0500

    Gets de ingresar movimientos

--- lib/screens/IngresarMovimientos/ingresar_movimientos_controller.dart
+++ lib/screens/IngresarMovimientos/ingresar_movimientos_controller.dart
@@ -1,24 +1,56 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../../entities/Tipo.dart';
 import '../../entities/Categoria.dart';
 
 class ingresar_movimientos_controller extends GetxController {
   Rx<Tipo?> selectedTipo = Rx<Tipo?>(null);
-  List<Tipo> tipos = [
-    Tipo(id: 1, name: 'Ingreso'),
-    Tipo(id: 2, name: 'Gasto'),
-    Tipo(id: 3, name: 'Ahorro'),
-  ];
+  RxList<Tipo> tipos = <Tipo>[].obs;
+  RxList<Categoria> categorias = <Categoria>[].obs;
 
-  List<Categoria> categorias = [
-    Categoria(id: 1, name: 'Comida', tipo: Tipo(id: 2, name: 'Gasto'), presupuesto: 240),
-    Categoria(id: 2, name: 'Transporte', tipo: Tipo(id: 2, name: 'Gasto'), presupuesto: 100),
-    Categoria(id: 3, name: 'Salud', tipo: Tipo(id: 2, name: 'Gasto'), presupuesto: 150),
-    Categoria(id: 4, name: 'Educación', tipo: Tipo(id: 2, name: 'Gasto'), presupuesto: 200),
-    Categoria(id: 5, name: 'Entretenimiento', tipo: Tipo(id: 2, name: 'Gasto'), presupuesto: 120),
-    Categoria(id: 6, name: 'Ingresos adicionales', tipo: Tipo(id: 1, name: 'Ingreso'), presupuesto: 500),
-    Categoria(id: 7, name: 'Ahorros futuros', tipo: Tipo(id: 3, name: 'Ahorro'), presupuesto: 300),
-  ];
+  @override
+  void onInit() {
+    super.onInit();
+    fetchTipos();
+    fetchCategorias();
+  }
+
+  Future<void> fetchTipos() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/api/tipo'));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      tipos.value = jsonResponse.map((item) => Tipo.fromJson(item)).toList();
+    } else {
+      Get.snackbar('Error', 'No se pudieron cargar los tipos');
+    }
+  }
+
+  Future<void> fetchCategorias() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/api/categoria'));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      categorias.value = jsonResponse.map((item) => adaptarCategoria(item)).toList();
+    } else {
+      Get.snackbar('Error', 'No se pudieron cargar las categorías');
+    }
+  }
+
+  Categoria adaptarCategoria(Map<String, dynamic> json) {
+    int id = json['id'];
+    String name = json['name'];
+    int tipoId = json['tipo_id']; // Usar tipo_id del JSON recibido
+    double presupuesto = double.parse(json['presupuesto'].toString()); // Asegurar que presupuesto sea un double
+
+    // Buscar el Tipo correspondiente en la lista tipos
+    Tipo? tipo = tipos.firstWhereOrNull((tipo) => tipo.id == tipoId);
+
+    if (tipo == null) {
+      throw Exception('No se encontró el tipo con id $tipoId');
+    }
+
+    return Categoria(id: id, name: name, tipo: tipo, presupuesto: presupuesto);
+  }
 
   void setSelectedTipo(Tipo? newValue) {
     selectedTipo.value = newValue;
